4100b22b27d98462a5946aba9929e8e4344c130a
Dheeraj-Royal <95634532+Dheeraj-Royal@users.noreply.github.com>
2022-01-20 20:00:33 +0530

    Add files via upload

---
+++ day3Assignment.py
@@ -0,0 +1,45 @@
+name=input("Enter your name :")
+age=int(input("Ente your age :"))
+height=float(input("Ente your height :"))
+quali=input("Enter your qualification :")
+marks=input("Enter your 10th marks :")
+pos=input("Enter your job qualification :")
+favFood=input("Enter your fav food :")
+book=input("Enter your fav book :")
+color=input("Enter your fav color :")
+game = input("Enter your fav game  :")
+
+print(f'\n\nName of the candidate is {name}')
+print(f"Details of {name} are as follows :")
+print(f'\tAge : {age}\n\tHeight : {height}\n\tQualification : {quali}\
+\n\tMarks(B.tech) : {marks}\n\tJob : {pos}\n\tFavouriteFood : {favFood}\
+\n\tfavBook : {book}\n\tfavColor : {color}\n\tfavGame : {game}')
+
+
+"""output:
+
+    Enter your name :Dheeraj
+    Ente your age :20
+    Ente your height :5.11
+    Enter your qualification :btech
+    Enter your 10th marks :970
+    Enter your job qualification :webDeVELOPER
+    Enter your fav food :chickenBiriyani
+    Enter your fav book :storyBooks
+    Enter your fav color :whiteANDblack
+    Enter your fav game  :chess
+
+
+    Name of the candidate is Dheeraj
+    Details of Dheeraj are as follows :
+	Age : 20
+	Height : 5.11
+	Qualification : btech
+	Marks(B.tech) : 970
+	Job : webDeVELOPER
+	FavouriteFood : chickenBiriyani
+	favBook : storyBooks
+	favColor : whiteANDblack
+	favGame : chess
+
+"""
